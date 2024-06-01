74038a2fa4e4dd241ca52a41b4f74d70ce543b05
dmitrmedv <dmitrmedv@gmail.com>
2024-06-01 21:03:08 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const container = document.querySelector(".container");
-const text = document.querySelector(".text");
-const restart = document.querySelector(".restart");
-const backdrop = document.querySelector(".backdrop");
-const winner = document.querySelector(".winner");
+const container = document.querySelector('.container');
+const text = document.querySelector('.text');
+const restart = document.querySelector('.restart');
+const backdrop = document.querySelector('.backdrop');
+const winner = document.querySelector('.winner');
 
-container.insertAdjacentHTML("beforeend", makeMurckup());
-const boxes = document.querySelectorAll(".box");
+container.insertAdjacentHTML('beforeend', makeMurckup());
+const boxes = document.querySelectorAll('.box');
 
 let userX = [];
 let userO = [];
-let marker = "X";
+let marker = 'X';
 let count = {
   userX: 0,
   userO: 0,
 };
 
-restart.addEventListener("click", reset);
+restart.addEventListener('click', reset);
 
 function showBunner(message) {
   console.log(12345);
-  backdrop.classList.remove("hidden");
+  backdrop.classList.remove('hidden');
   winner.textContent = message;
 }
 
 function makeMurckup() {
-  let murckup = "";
+  let murckup = '';
   for (let i = 0; i < 9; i += 1) {
     murckup += `<div class="box" data-id=${i}></div>`;
   }
@@ -32,11 +32,11 @@ function makeMurckup() {
 }
 
 function reset() {
-  backdrop.classList.add("hidden");
-  boxes.forEach((item) => (item.innerHTML = ""));
+  backdrop.classList.add('hidden');
+  boxes.forEach(item => (item.innerHTML = ''));
   userX = [];
   userO = [];
-  marker = "X";
+  marker = 'X';
 }
 
 const winnerCombinations = [
@@ -50,33 +50,29 @@ const winnerCombinations = [
   [2, 4, 6],
 ];
 
-container.addEventListener("click", onClick);
+container.addEventListener('click', onClick);
 
 function onClick(e) {
-  if (e.target.classList.contains("box") && e.target.textContent === "") {
+  if (e.target.classList.contains('box') && e.target.textContent === '') {
     e.target.textContent = marker;
-    if (marker === "X") {
+    if (marker === 'X') {
       userX.push(Number(e.target.dataset.id));
     } else {
       userO.push(Number(e.target.dataset.id));
     }
-    marker === "X" ? (marker = "O") : (marker = "X");
+    marker === 'X' ? (marker = 'O') : (marker = 'X');
   }
 
-  if (
-    winnerCombinations.some((item) => item.every((el) => userX.includes(el)))
-  ) {
-    showBunner("Виграв гравець Х");
+  if (winnerCombinations.some(item => item.every(el => userX.includes(el)))) {
+    showBunner('Виграв гравець Х');
     return;
   }
-  if (
-    winnerCombinations.some((item) => item.every((el) => userO.includes(el)))
-  ) {
-    showBunner("Виграв гравець О");
+  if (winnerCombinations.some(item => item.every(el => userO.includes(el)))) {
+    showBunner('Виграв гравець О');
     return;
   }
   if (userX.length === 5 || userX.length === 5) {
-    showBunner("Нічия 😵");
+    showBunner('Нічия 😵');
     return;
   }
 }
